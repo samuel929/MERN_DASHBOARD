e66e7986c3a49a409c9ab2513247815de7244534
Samuel <43027737+samuel929@users.noreply.github.com>
2023-03-08 06:05:12 +0200

    added api endpoint

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 
 export const api=createApi({
-    baseQuery:fetchBaseQuery({baseUrl:"http://localhost:5001"}),
+    baseQuery:fetchBaseQuery({baseUrl:"https://admin-backend-2b1s.onrender.com"}),
     reducerPath:"adminApi",
     tagTypes:["User","Products","Customers","Transactions","Geography","SALES","ADMINS",
     "Performance",
